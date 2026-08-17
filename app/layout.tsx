b99b74b2acd3ad58fd56dcddc742dc44793fa4a6
jovankawilyam <jovankamuzaki1@gmail.com>
2026-08-17 19:34:54 +0700

    feat: update site metadata and add OpenGraph configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,30 @@ import "./globals.css";
 const fontClassName = "font-sans";
 
 export const metadata: Metadata = {
-  title: "Doclang Boba",
-  description: "Layanan Dokumen Pasca Lelang KPKNL Bogor",
+  title: "Dokumen Pasca Lelang Bogor Bageur",
+  description: "Doclang Boba",
+  keywords: [
+    "Dokumen Pasca Lelang KPKNL Bogor Bageur",
+    "Layanan Dokumen Pasca Lelang KPKNL Bogor Bageur",
+    "Dokumen Pasca Lelang KPKNL Bogor Bageur",
+    "Layanan Dokumen Pasca Lelang KPKNL Bogor Bageur",
+  ],
+  openGraph: {
+    title: "Doclang Boba",
+    description: "Dokumen Pasca Lelang KPKNL Bogor Bageur",
+    url: "",
+      siteName: "Doclang Boba",
+    images: [
+      {
+        url: "/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Doclang Boba",
+      },
+    ],
+    locale: "id_ID",
+    type: "website",
+  },
 };
 
 export default function RootLayout({
